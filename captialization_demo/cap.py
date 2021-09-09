e2028c1307ab11d1c345060198dd1aca44a423ad
Christoffer Gray <christoffergray@gmail.com>
2021-09-09 00:18:19 -0700

    UnitTest Library Test
    During our lecture we were introduced to using UnitTest as a way to debug code.

---
+++ captialization_demo/cap.py
@@ -0,0 +1,11 @@
+'''
+All this module does it receives a string modifies it
+'''
+def cap_text(text):
+    '''
+    Input a string
+    Output the capitalized string
+    '''
+    #return (text.capitalize()) 
+    # This is only captializing the first letter of the string
+    return (text.title())
---
+++ captialization_demo/test_cap.py
@@ -0,0 +1,22 @@
+'''
+This is where we will be running all the code
+What we are attempting to do is run UnitTest to check our code
+'''
+#Import Libraries
+import unittest
+import cap
+
+class TestCap(unittest.TestCase):
+    
+    def test_one_word(self):
+        text = 'python'
+        result = cap.cap_text(text)
+        self.assertEqual(result,'Python')
+
+    def test_multiple_words(self):
+        text = 'monty python'
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Monty Python')
+
+if __name__ == '__main__':
+    unittest.main()
